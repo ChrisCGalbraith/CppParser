f46e9142cdf2a9f3c08dd8b0afeed0361790d5a2
Chris Galbraith <chriscgalbraith@gmail.com>
2020-02-19 15:04:06 +0000

    Removed example.h and changed instance_mc to .header

--- main.py
+++ main.py
@@ -3,41 +3,51 @@ import sys
 import os
 
 try:
-    cppHeader = CppHeaderParser.CppHeader("C:\Development\PycharmProjects\CppParser\instance_molten_core.cpp")
+    cppHeader = CppHeaderParser.CppHeader("C:\Development\PycharmProjects\CppParser\instance_molten_core.h")
 except CppHeaderParser.CppParseError as e:
     print(e)
     sys.exit(1)
-
-curr_class = cppHeader.classes["npc_firewalker"]
-
-
-# This is the path where you want to search
-my_path = r'C:\Development\PycharmProjects'
-my_file_path = os.path.dirname(__file__)
-# this is extension you want to detect
-my_extension = '.cpp'   # this can be : .jpg  .png  .xls  .log .....
 
 
 def find_files(path, extension):
     for root, dirs_list, files_list in os.walk(path):
         for file_name in files_list:
             if os.path.splitext(file_name)[-1] == extension:
-                file_name_path = os.path.join(root, file_name)
+                list_all_files.append(file_name)
+                # file_name_path = os.path.join(root, file_name)
                 print(file_name)
-                print(file_name_path)   # This is the full path of the filter file
+                # print(file_name_path)   # This is the full path of the filter file
 
 
 def create_gtest_templates():
-    list_method_names = CppHeaderParser.CppClass.get_all_method_names(curr_class)
-    with open(my_file_path + '/test/test_myproject.cpp', 'w') as f:
-        for x in list_method_names:
-            t1 = str(x)
-            t2 = "WHAT_WE_ARE_TESTING"
-            template = "TEST(" + t1 + ", " + t2 + ")\n{\n // This is a placeholder test-function for " + t1 + "()\n}"
-            f.write(template + "\n\n")
+    for y in list_all_classes:
+        new_method_list = CppHeaderParser.CppClass.get_all_method_names(cppHeader.classes[y])
+        with open(my_file_path + '/test/test_myproject.cpp', 'a') as f:
+            for x in new_method_list:
+                t1 = str(x)
+                t2 = "WHAT_WE_ARE_TESTING"
+                template = "TEST(" + t1 + ", " + t2 + ")\n{\n // This is a placeholder test-function for " + t1 + "()\n}"
+                f.write(template + "\n\n")
 
 
-find_files('C:/Development/PycharmProjects', '.cpp')
+def get_classes():
+    for i in cppHeader.classes:
+        list_all_classes.append(i)
+
+
+list_all_classes = []  # Initialise empty container list for classes within a file
+
+get_classes()  # Populate list_all_classes[]
+
+my_file_path = os.path.dirname(__file__)  # Directory containing main.py
+
+list_all_files = []  # Initialise empty container list for header files below my_file_path in hierarchy
+
+find_files(my_file_path, '.h')  # Populates list_all_files[] with .h files it has found
+
 create_gtest_templates()
+print(list_all_classes)
 
-print(my_file_path)
+
+
+
---
+++ test/test_myproject.cpp
@@ -0,0 +1,130 @@
+TEST(instance_molten_core, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for instance_molten_core()
+}
+
+TEST(GetInstanceScript, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for GetInstanceScript()
+}
+
+TEST(instance_molten_core_InstanceMapScript, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for instance_molten_core_InstanceMapScript()
+}
+
+TEST(OnPlayerEnter, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for OnPlayerEnter()
+}
+
+TEST(OnCreatureCreate, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for OnCreatureCreate()
+}
+
+TEST(OnGameObjectCreate, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for OnGameObjectCreate()
+}
+
+TEST(OnCreateFireCircleObject, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for OnCreateFireCircleObject()
+}
+
+TEST(OnCreateRuneObject, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for OnCreateRuneObject()
+}
+
+TEST(UseRune, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for UseRune()
+}
+
+TEST(SetData, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for SetData()
+}
+
+TEST(GetData, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for GetData()
+}
+
+TEST(GetData64, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for GetData64()
+}
+
+TEST(SetBossState, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for SetBossState()
+}
+
+TEST(SummonMajordomoExecutus, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for SummonMajordomoExecutus()
+}
+
+TEST(CheckMajordomoExecutus, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for CheckMajordomoExecutus()
+}
+
+TEST(GetSaveData, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for GetSaveData()
+}
+
+TEST(Load, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for Load()
+}
+
+TEST(go_molten_core_rune, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for go_molten_core_rune()
+}
+
+TEST(OnGossipHello, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for OnGossipHello()
+}
+
+TEST(npc_firewalker, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for npc_firewalker()
+}
+
+TEST(GetAI, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for GetAI()
+}
+
+TEST(npc_firewalkerAI, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for npc_firewalkerAI()
+}
+
+TEST(Reset, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for Reset()
+}
+
+TEST(JustDied, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for JustDied()
+}
+
+TEST(EnterCombat, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for EnterCombat()
+}
+
+TEST(UpdateAI, WHAT_WE_ARE_TESTING)
+{
+ // This is a placeholder test-function for UpdateAI()
+}
+
