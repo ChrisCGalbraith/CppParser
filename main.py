3406d47360361664312b77a91839e93bb0e6eb43
Christopher Galbraith <39371221+ChrisCGalbraith@users.noreply.github.com>
2020-03-31 21:33:40 +0100

    Add files via upload
    Option to choose folders to exclude

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import CppHeaderParser
 import os
+import argparse
 
 
-def find_files(path, extension, extension2):
-    for root, dirs_list, files_list in os.walk(path):
+def find_files(path, extension, extension2, excludes):
+    for root, dirs_list, files_list in os.walk(path, topdown=True):
+        # Removes specified directories from os.walk,
+        dirs_list[:] = [d for d in dirs_list if d not in excludes]
         for file_name in files_list:
             if os.path.splitext(file_name)[-1] == extension:
                 list_all_files.append(file_name)
@@ -13,7 +16,8 @@ def find_files(path, extension, extension2):
                 list_all_test_file_paths.append(root + "/" + file_name)
 
 
-def get_classes():
+def main():
+    print("Processing...")
     for x, idx in enumerate(list_all_file_paths):
         # Assigns cppheader file to current file in the list
         cppheader = CppHeaderParser.CppHeader(idx)
@@ -22,6 +26,7 @@ def get_classes():
 
         # Populate list of classes from our current cppheader file
         for i in cppheader.classes:
+            # print("HERE WE ARE " + str(cppheader.variables))
             list_all_classes.append(i)
 
         mod_list = list()
@@ -30,17 +35,17 @@ def get_classes():
             new_ext = os.path.splitext(j)[0] + ".cpp"
             mod_list.append(new_ext)
 
-        # Check if test_x.cpp exists, if it does not this creates the file and adds includes.
-        if not os.path.exists(my_file_path + '/test/' + mod_list[x]):
+        # Check if test_x.cpp exists, if it does not, this creates the file and adds includes.
+        if not os.path.exists(my_file_path + '/test/test_' + mod_list[x]):
             # Adding the includes, handles both " " and < > cases
-            with open(my_file_path + '/test/' + mod_list[x], 'a') as f:
+            with open(my_file_path + '/test/test_' + mod_list[x], 'a') as f:
                 f.write("#define GTEST_COUT std::cerr << [          ] [ WARNING ]" "\n")
                 # Puts includes at the top of the file
                 for incl in cppheader.includes:
                     f.write("#include " + " %s" % incl + "\n")
                 f.write("\n")
 
-        with open(my_file_path + '/test/' + mod_list[x], 'a') as f:
+        with open(my_file_path + '/test/test_' + mod_list[x], 'a+') as f:
             for y in list_all_classes:
                 # Creates a list of methods from current cppheader
                 new_method_list = (CppHeaderParser.CppClass.get_all_method_names(cppheader.classes[y]))
@@ -48,16 +53,14 @@ def get_classes():
                 create_curr_test_list()
                 for i in new_method_list:
                     m1 = str(i)
-                    list_of_comp_method_strings.append(y + "::" + m1)
+                    list_of_comp_method_strings.append(m1)
                 new_meths = list_check(list_of_comp_method_strings, old_methods)
                 if not new_meths:
                     pass
                 else:
                     for z in new_meths:
-                        t1 = str(z)
-                        # This will be some sort of return type perhaps
-                        t2 = "WHAT_WE_ARE_TESTING"
-
+                        t1 = "test_" + str(y)
+                        t2 = str(z)
                         # Template that matches GTest's basic function TEST(class_function, what_we_are_testing)
                         template = "TEST(" + t1 + ", " + t2 + ")\n{\n GTEST_COUT << This test has not been written yet! << std::endl; \n}"
                         f.write(template + "\n\n")
@@ -65,11 +68,12 @@ def get_classes():
 
 def create_curr_test_list():
     for x, idx in enumerate(list_all_test_file_paths):
-        with open (list_all_test_file_paths[x], 'r') as r:
+        with open(list_all_test_file_paths[x], 'r') as r:
             for line in r:
                 if line.startswith("TEST("):
                     s = line[5:]
-                    old_methods.append(s.split(',')[0])
+                    # Appends the class string, removing )\n and whitespaces
+                    old_methods.append(s.split(',')[1][1:-2])
 
 
 # Compares list of function names in source file to current test file, returns missing functions to be added
@@ -77,13 +81,32 @@ def list_check(list_x, list_y):
     if not list(set(list_x) - set(list_y)):
         pass
     else:
+        # print("This is list x: " + str(list_x))
+        # print("This is list y: " + str(list_y))
         print("New methods added are -> " + str(list(set(list_x) - set(list_y))))
     return list(set(list_x) - set(list_y))
 
 
-my_file_path = os.path.dirname(__file__)  # Directory containing main.py
+def get_inputs():
+    excludes = list()
+    print("================================================================================")
+    print("This is the menu to select folders you want CppParser to ignore.")
+    print("You will be asked to enter the name of each folder, one by one, that you wish to skip.")
+    print("Folders must be below the current working directory (CWD) in hierarchy, and when specified, nothing within will be parsed.")
+    print("================================================================================")
+    print("Enter folder names exactly as seen in file explorer.")
+    print("To finish, enter the word || done || or leave blank and press Enter.\n")
+    while True:
+        i = input("Enter name of folder(s) below CWD to exclude: ")
+        if i.strip() == 'done' or 'none':
+            return excludes
+        else:
+            excludes.append(i)
 
-list_all_test_files = [] # Initialise empty container list for test cpp files
+
+my_file_path = os.path.dirname(os.path.abspath(__file__))   # os.path.dirname(__file__)  # Directory containing main.py
+
+list_all_test_files = []  # Initialise empty container list for test cpp files
 
 list_all_files = []  # Initialise empty container list for header files below my_file_path in hierarchy
 
@@ -93,14 +116,15 @@ list_all_file_paths = []  # Initialise empty container list for full paths to he
 
 old_methods = []  # Initialise container for methods found when scanning directories
 
-# Main functions being called
-find_files(my_file_path, '.h', '.cpp')  # Populates list_all_files[] with .h files it has found
-
 create_curr_test_list()
 
-get_classes()
+find_files(my_file_path, '.h', '.cpp', get_inputs())  # Populates list_all_files[] with .h files it has found
 
+if __name__ == '__main__':
+    main()
 
+print("\n")
+print("Program has finished!")
 
 
 
